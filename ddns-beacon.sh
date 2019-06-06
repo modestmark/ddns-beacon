ae636f469ba6dd49c6c62217f2376a41e82ae219
mark gagnon <reffstar@gmail.com>
2019-06-06 00:26:36 -0600

    constructed url string works to update digitalocean record, now make curl like it

--- ddns-beacon.sh
+++ ddns-beacon.sh
@@ -24,11 +24,27 @@ fi
 #read in variables from config file
 configData=`cat ./ddns-beacon-config`
 registrar=`echo $configData | awk '{print $1}'`
-apiKey=`echo $configData | awk '{print $2}'`
-recordID=`echo $configData | awk '{print $3}'`
+domainName=`echo $configData | awk '{print $2}'`
+apiKey=`echo $configData | awk '{print $3}'`
+recordID=`echo $configData | awk '{print $4}'`
 
 #find, format, and store wan ip
-ddnsip=`host myip.opendns.com resolver1.opendns.com | grep "has address" | awk '{print $4}'`
+#ddnsip=`host myip.opendns.com resolver1.opendns.com | grep "has address" | awk '{print $4}'`
+ddnsip=8.8.8.8
+
+#construct URL strings for DNS record update via API
+if [ $registrar="DigitalOcean" ]
+  then
+    recordURL="\"https://api.digitalocean.com/v2/domains/"
+    recordURL=${recordURL}$domainName
+    recordURL=${recordURL}/records/
+    recordURL=${recordURL}$recordID\"
+    echo $recordURL
+
+    payload="'{\"data\":\""
+    payload=${payload}$ddnsip
+    payload=${payload}\"\}\'
+fi
 
 #compare to ip from last run, set update bit if change has occurred
 oldddnsip=`cat ./oldddnsip`
@@ -39,10 +55,13 @@ if [ $oldddnsip != $ddnsip ]
   else
     update=0
 fi
+
+#test stuff, delete when done
 echo $ddnsip
+echo curl -X PUT -H \"Content-Type: application/json\" -H \"Authorization: Bearer $apiKey\" -d $payload "$recordURL"
 
 #if update bit is set, update DNS record
 if [ $update == 1 ]
   then
-
-
+    curl -X PUT -H "Content-Type: application/json" -H "Authorization: Bearer $apiKey" -d $payload $recordURL
+fi
